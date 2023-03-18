2d35d2fe65ea00b9b0ffdf1cd748a6727e31b0d5
lvaiqian <114115976+lvaiqian@users.noreply.github.com>
2023-03-18 14:34:56 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -71,7 +71,7 @@ export const USER_CONFIG = {
       // 所在城市
       city: "晋中",
       // 新历生日, 仅用作获取星座运势, 格式必须
-      horoscopeDate: '01-16',
+      horoscopeDate: '05-19',
       // 获取什么时候的星座运势，可选：['今日', '明日', '本周', '本月', '今年'], 留空则随机
       horoscopeDateType: '今日',
       // 他点击详情后跳转的页面,你可以设置成微博的热榜，也可以设置成其他，网址一定要填对；不填对也没关系，随便你，会打不开而已。
@@ -79,16 +79,16 @@ export const USER_CONFIG = {
       // 专属节日提醒，如果你在这里填写了节日提醒，就不会执行FESTIVALS的提醒了, 和FESTIVALS的配置方法相同，可以往下查看，我这里就不重复写了
       festivals: [
         // 注意：此条配置日期为阴历日期，因为`type`中 “生日” 之前有 * 符号
-        {"type": "*生日", "name": "老婆", "year": "2006", "date": "01-16"},
-        {"type": "节日", "name": "结婚纪念日", "year": "2020", "date": "09-03"},
+        {"type": "*生日", "name": "老婆", "year": "2006", "date": "05-19"},
+        {"type": "节日", "name": "结婚纪念日", "year": "2020", "date": "05-19"},
         // 注意：此条配置日期为阳历日期，因为`type`中 “生日” 之前没有 * 符号
-        {"type": "生日", "name": "李四", "year": "1996", "date": "09-31"},
-        {"type": "节日", "name": "被搭讪纪念日", "year": "2021", "date": "09-01"},
+        {"type": "生日", "name": "李四", "year": "1996", "date": "05-19"},
+        {"type": "节日", "name": "被搭讪纪念日", "year": "2021", "date": "05-19"},
       ],
       // 专属纪念日/倒数日，如果你在这里填写了纪念日/倒数日，就不会计算CUSTOMIZED_DATE_LIST的日子了, 和CUSTOMIZED_DATE_LIST的配置方法相同，可以往下查看，我这里就不重复写了
       customizedDateList: 
         // 在一起的日子
-        [{"keyword": "love_day", date: "2023-01-01"}],
+        [{"keyword": "love_day", date: "2023-05-19"}],
        
     },
     {
@@ -97,11 +97,11 @@ export const USER_CONFIG = {
       useTemplateId: "LzI1G2A9ZzI_j14Lt25tknvzL6akKOgOvzhoe3U1eic",
       province: "山西",
       city: "晋中",
-      horoscopeDate: '01-16',
+      horoscopeDate: '05-19',
       horoscopeDateType: '今日',
       openUrl: "https://wangxinleo.cn",
-      festivals: [ {"type": "*生日", "name": "婷婷", "year": "2006", "date": "01-16"}],
-      customizedDateList: [{"keyword": "love_day", date: "2023-01-01"}]
+      festivals: [ {"type": "*生日", "name": "婷婷", "year": "2006", "date": "05-19"}],
+      customizedDateList: [{"keyword": "love_day", date: "2023-05-19"}]
     },
     {
       name: "付小姐请查收您的每日推送",
@@ -113,7 +113,7 @@ export const USER_CONFIG = {
       horoscopeDateType: '今日',
       openUrl: "https://wangxinleo.cn",
       festivals: [ {"type": "*生日", "name": "付小姐", "year": "2003", "date": "05-19"}],
-      customizedDateList: [{"keyword": "love_day", date: "2023-01-01"}]
+      customizedDateList: [{"keyword": "love_day", date: "2023-05-19"}]
     },
     {
       name: "付小姐",
@@ -125,7 +125,7 @@ export const USER_CONFIG = {
       horoscopeDateType: '今日',
       openUrl: "https://wangxinleo.cn",
       festivals: [{"type": "*生日", "name": "付小姐", "year": "2003", "date": "05-19"}],
-      customizedDateList: [{"keyword": "love_day", date: "2023-01-01"}]
+      customizedDateList: [{"keyword": "love_day", date: "2023-05-19"}]
     },
     // 你可以不断按格式往下增加
     // ...
